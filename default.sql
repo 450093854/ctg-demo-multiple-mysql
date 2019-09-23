3ef8a0f7f3eec1705a69c8fb21fb313aa2a37245
450093854 <450093854@qq.com>
2019-09-23 19:11:07 +0800

    Update default.sql

--- default.sql
+++ default.sql
@@ -1,21 +1,35 @@
+/*
+ Navicat Premium Data Transfer
+ Source Server         : mysql-localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50717
+ Source Host           : localhost:3306
+ Source Schema         : pipeline-test
+ Target Server Type    : MySQL
+ Target Server Version : 50717
+ File Encoding         : 65001
+ Date: 06/04/2019 15:11:19
+*/
 
-DROP DATABASE IF EXISTS `template`;
-CREATE DATABASE `template`;
-USE `template`;
+set character set utf8;
 
-SET FOREIGN_KEY_CHECKS=0;
+drop database if exists `pipeline-test`;
 
-DROP TABLE IF EXISTS `template`;
-CREATE TABLE `template` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `language` varchar(50) NOT NULL,
-  `framework` varchar(50) NOT NULL,
-  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+create database `pipeline-test`  DEFAULT CHARACTER SET utf8;
 
-INSERT INTO `template` VALUES ('1', 'python', 'flask', '2018-01-26 01:46:36');
-INSERT INTO `template` VALUES ('2', 'golang', 'beego', '2018-01-26 01:46:36');
-INSERT INTO `template` VALUES ('3', 'java', 'spring', '2018-01-26 01:46:36');
-INSERT INTO `template` VALUES ('4', 'nodejs', 'express', '2018-01-26 01:46:36');
-INSERT INTO `template` VALUES ('5', 'php', 'laravel', '2018-01-26 01:46:36');
+use `pipeline-test`
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `user_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '用户名',
+  `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `mobile` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '手机号',
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
